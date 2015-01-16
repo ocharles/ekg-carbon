376ace8967d42c0fb0f7002d65d8eaf8cb76d500
Oliver Charles <ollie@ocharles.org.uk>
2015-01-16 19:36:03 +0000

    Regenerate default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,15 @@
-{ cabal, ekgCore, network, networkCarbon, text, vector }:
-cabal.mkDerivation (self: {
+{ mkDerivation, base, ekg-core, network, network-carbon, stdenv
+, text, time, unordered-containers, vector
+}:
+mkDerivation {
   pname = "ekg-carbon";
-  version = "1.0.0";
+  version = "1.0.2";
   src = ./.;
-  buildDepends = [ ekgCore network networkCarbon text vector ];
-})
+  buildDepends = [
+    base ekg-core network network-carbon text time unordered-containers
+    vector
+  ];
+  homepage = "http://github.com/ocharles/ekg-carbon";
+  description = "An EKG backend to send statistics to Carbon (part of Graphite monitoring tools)";
+  license = stdenv.lib.licenses.bsd3;
+}
