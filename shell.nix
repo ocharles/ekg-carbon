cd7a834fb40a9290371642e25cc217323e52bd0f
Ollie Charles <ollie@ocharles.org.uk>
2017-02-18 09:57:22 +0000

    Refresh shell.nix

--- shell.nix
+++ shell.nix
@@ -1,9 +1,31 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
 let
-  pkgs = import <nixpkgs> {};
-  haskellPackages = pkgs.haskellngPackages.override {
-    overrides = self: super: {
-      ekg-carbon = self.callPackage ./. {};
-      network-carbon = self.callPackage ../network-carbon {};
-    };
-  };
-in haskellPackages.ekg-carbon.env
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, ekg-core, network, network-carbon
+      , stdenv, text, time, unordered-containers, vector
+      }:
+      mkDerivation {
+        pname = "ekg-carbon";
+        version = "1.0.7";
+        src = ./.;
+        libraryHaskellDepends = [
+          base ekg-core network network-carbon text time unordered-containers
+          vector
+        ];
+        homepage = "http://github.com/ocharles/ekg-carbon";
+        description = "An EKG backend to send statistics to Carbon (part of Graphite monitoring tools)";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
